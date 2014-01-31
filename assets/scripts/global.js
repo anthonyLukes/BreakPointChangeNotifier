c9d40d7f8adbb4d38787ca86bc6a28c34ae20e28
Anthony Lukes <anthony.lukes@gmail.com>
2014-01-30 22:49:21 -0600

    adding yuidoc comments

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -1,40 +1,75 @@
-/* ---------------------------------------------------------------------
-Global JavaScript
-
-Target Browsers: All
-Authors: Anthony Lukes
------------------------------------------------------------------------- */
-
-// Namespace Object
+/**
+ * Namespace Object
+ */
 var NERD = NERD || {};
 
-// Pass reference to jQuery and Namespace
+/**
+ * Pass reference to jQuery and Namespace
+ */
 (function($, APP) {
-
-    // DOM Ready Function
+    /**
+     * DOM Ready Function
+     */
     $(function() {
         APP.BreakpointChangeNotifier.init();
         APP.MainMenuToggler.init();
     });
 
-/* ---------------------------------------------------------------------
-BreakpointChangeNotifier
-Author: Anthony Lukes
-
------------------------------------------------------------------------- */
+/**
+ * Event system that publishes when a user resizes window
+ * across responsive breakpoint threshholds. Reads CSS for
+ * content: '' attribute on body:after psuedo element.
+ * 
+ * @class BreakpointChangeNotifier
+ * @static
+ */
 APP.BreakpointChangeNotifier = {
+    /**
+     * @property previousSize
+     * @type String
+     */
     previousSize: null,
+    /**
+     * @property currentSize
+     * @type String
+     */
     currentSize: null,
+    /**
+     * @property BREAKPOINT_CHANGE_EVENT
+     * @type String
+     * @final
+     */
     BREAKPOINT_CHANGE_EVENT: 'onBreakpointChange',
+    /**
+     * @property SMALL
+     * @type String
+     * @final
+     */
     SMALL: 'small',
+    /**
+     * @property MEDIUM
+     * @type String
+     * @final
+     */
     MEDIUM: 'medium',
+    /**
+     * @property LARGE
+     * @type String
+     * @final
+     */
     LARGE: 'large',
+    /**
+     * @method init
+     */
     init: function() {
         if (window.getComputedStyle) {
             this.currentSize = this.getCurrentSize();
             this.bindResize();
         }
     },
+    /**
+     * @method bindResize
+     */
     bindResize: function() {
         var self = this;
         $(window).resize(function() {
@@ -42,9 +77,16 @@ APP.BreakpointChangeNotifier = {
             self.handleResize(currentSize);
         });
     },
+    /**
+     * @method getCurrentSize
+     */
     getCurrentSize: function() {
         return window.getComputedStyle(document.body,':after').getPropertyValue('content');
     },
+    /**
+     * @method handResize
+     * @param {String} currentSize The current size detected from CSS
+     */
     handleResize: function(currentSize) {
         if (this.currentSize !== currentSize) {
             this.previousSize = this.currentSize;
@@ -52,20 +94,34 @@ APP.BreakpointChangeNotifier = {
             this.notifyChange({currentSize: this.currentSize, previousSize: this.previousSize});
         }
     },
+    /**
+     * @method notifyChange
+     * @param {Object} params Object holds attributes for previous and current breakpoint values
+     */
     notifyChange: function(params) {
         var BREAKPOINT_CHANGE_EVENT = this.BREAKPOINT_CHANGE_EVENT;
         $(document).trigger(BREAKPOINT_CHANGE_EVENT, [params]);
     }
 };
 
-/* ---------------------------------------------------------------------
-MainMenuToggler
-Author: Anthony Lukes
-
------------------------------------------------------------------------- */
+/**
+ * @class MainMenuToggler
+ * @static
+ */
 APP.MainMenuToggler = {
+    /**
+     * $mainMenu
+     * @type {Object}
+     */
     $mainMenu: null,
+    /**
+     * $menuToggler
+     * @type {Object}
+     */
     $menuToggler: null,
+    /**
+     * @method init
+     */
     init: function() {
         var $mainMenu  = $(document.getElementById('js-navMain'));
         if ($mainMenu.length === 0)
@@ -82,6 +138,9 @@ APP.MainMenuToggler = {
         }
         this.bindListener();
     },
+    /**
+     * @method bindListener
+     */
     bindListener: function() {
         var self = this;
         $(document).on(APP.BreakpointChangeNotifier.BREAKPOINT_CHANGE_EVENT, function(e, params) {
@@ -93,6 +152,9 @@ APP.MainMenuToggler = {
             }
         });
     },
+    /**
+     * @method enable
+     */
     enable: function() {
         var self = this;
         this.$menuToggler.show().on('click', function() {
@@ -100,10 +162,16 @@ APP.MainMenuToggler = {
         });
         this.$mainMenu.hide();
     },
+    /**
+     * @method disable
+     */
     disable: function() {
         this.$menuToggler.hide().off();
         this.$mainMenu.show();
     },
+    /**
+     * @method toggleMenu
+     */
     toggleMenu: function() {
         this.$mainMenu.slideToggle();
     }
